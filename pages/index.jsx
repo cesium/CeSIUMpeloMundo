39174d5db6887fb373c150bb0336019dbb941c57
Nelson Estevão <nelsonmestevao@proton.me>
2022-06-13 15:49:49 +0100

    Move data to JSON file

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,9 @@
 import dynamic from 'next/dynamic';
 
+import pins from '~/data/places.json';
+
 const Map = dynamic(() => import('~/components/Map'), { ssr: false });
 
 export default function Home() {
-  return <Map />;
+  return <Map pins={pins} />;
 }
