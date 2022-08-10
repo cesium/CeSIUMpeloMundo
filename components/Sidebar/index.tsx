74a87748c09aaa1c1aa1a3949b9b6d23de62ef5f
Diogo Matos <diogomatosemail@gmail.com>
2022-08-10 20:28:48 +0100

    Added sidebar

---
+++ components/Sidebar/index.tsx
@@ -0,0 +1,64 @@
+import styles from './style.module.css';
+import {
+  getFullDateString,
+  getRelativeTimeString,
+  getNameString
+} from '~/lib/utils';
+import { useMemo } from 'react';
+import Image from 'next/image';
+
+type PinType = 'sticker' | 'picture' | 'special';
+
+export interface Pin {
+  author: string;
+  city: string;
+  country: string;
+  coordinates: [number, number];
+  date: string;
+  photo: string;
+  type?: PinType;
+  streetview?: string;
+}
+
+const Location = ({
+  type,
+  coordinates,
+  city,
+  country,
+  author,
+  photo,
+  date
+}: Pin) => {
+  const name = useMemo(() => getNameString(author), [author]);
+  return (
+    <div className={styles.listings}>
+      <b>
+        {' '}
+        {city}, {country}{' '}
+      </b>
+      <br></br>
+      {date && (
+        <i>
+          {getFullDateString(date)} ({getRelativeTimeString(date)})
+        </i>
+      )}
+      <br />
+      <span>{name}</span>
+    </div>
+  );
+};
+
+export default function Sidebar({ pins }: { pins: Pin[] }) {
+  return (
+    <div className={styles.list}>
+      <div className={styles.heading}>
+        <Image src="/images/cesium.png" width={180} height={61} />
+      </div>
+      <div>
+        {pins.map((pin: Pin) => (
+          <Location key={`${pin.coordinates}-${pin.author}`} {...pin} />
+        ))}
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,21 @@ import { ThemeProvider } from '~/hooks/useTheme';
 import type { Pin } from '~/components/Marker';
 
 import pins from '~/data/places.json';
+import Sidebar from '~/components/Sidebar';
 
 const Map = dynamic(() => import('~/components/Map'), { ssr: false });
 
 export default function Home() {
   return (
-    <ThemeProvider>
-      <Map pins={pins as Pin[]} />
-    </ThemeProvider>
+    <div>
+      <div className="sidebar" style={{ width: '20%' }}>
+        <Sidebar pins={pins as Pin[]} />
+      </div>
+      <div className="map" style={{ marginLeft: '20%' }}>
+        <ThemeProvider>
+          <Map pins={pins as Pin[]} />
+        </ThemeProvider>
+      </div>
+    </div>
   );
 }
