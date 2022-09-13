e6e54367663811d690b3ba42c3bcad519a1f8c7f
Diogo Matos <diogomatosemail@gmail.com>
2022-09-13 17:43:25 +0100

    Improve `makeLeaderboard()` algorithm

--- components/Leaderboard/Leaderboard.tsx
+++ components/Leaderboard/Leaderboard.tsx
@@ -1,37 +1,61 @@
 import styles from './style.module.css';
 import { PLACES } from '~/data/places';
 import { sortByOldest } from '~/lib/utils';
+import { IPin } from '~/lib/types';
 
 interface Player {
   author: string;
+  username: string;
   pins: number;
 }
 
-function makeLeaderboard(places) {
-  const authors = [];
-  const leaderboard = [];
+interface User {
+  author: string;
+  username: string;
+}
 
-  const sortedPlaces = places.sort(sortByOldest);
+function getSet(arr: User[]) {
+  return [...new Map(arr.map((item) => [item['username'], item])).values()];
+}
+
+function makeLeaderboard(places: IPin[]) {
+  const users = [];
+  const leaderboard = [];
+  const places_copy = places.slice();
+  const sortedPlaces = places_copy.sort(sortByOldest);
 
   for (var i = 0; i < sortedPlaces.length; i++) {
-    if (Array.isArray(sortedPlaces[i].author)) {
-      for (var j = 0; j < sortedPlaces[i].author.length; j++) {
-        authors.push(sortedPlaces[i].author[j]);
+    if (Array.isArray(sortedPlaces[i].username)) {
+      for (var j = 0; j < sortedPlaces[i].username.length; j++) {
+        users.push({
+          author: sortedPlaces[i].author[j],
+          username: sortedPlaces[i].username[j]
+        });
       }
     } else {
-      authors.push(sortedPlaces[i].author);
+      users.push({
+        author: sortedPlaces[i].author,
+        username: sortedPlaces[i].username
+      });
     }
   }
-  const authorsSet = [...new Set(authors)];
-  for (var i = 0; i < authorsSet.length; i++) {
+
+  const usersSet = getSet(users);
+
+  for (var i = 0; i < usersSet.length; i++) {
     var acc = 0;
-    for (var j = i; j < authors.length; j++) {
-      if (authorsSet[i] === authors[j]) {
+    for (var j = i; j < users.length; j++) {
+      if (usersSet[i].username === users[j].username) {
         acc++;
       }
     }
-    leaderboard.push({ author: authorsSet[i], pins: acc });
+    leaderboard.push({
+      author: usersSet[i].author,
+      username: usersSet[i].username,
+      pins: acc
+    });
   }
+
   return leaderboard;
 }
 
@@ -68,6 +92,22 @@ function getWidth(index: number, leaderboard: Player[]) {
   return (pins * 100) / maxPins + '%';
 }
 
+function getUsername(index: number, leaderboard: Player[]) {
+  const author = leaderboard[index].author;
+
+  var acc = 0;
+  for (var i = 0; i < leaderboard.length; i++) {
+    if (leaderboard[i].author === author) {
+      acc++;
+    }
+  }
+  if (acc > 1) {
+    return '@' + leaderboard[index].username;
+  } else {
+    return '';
+  }
+}
+
 export default function Leaderboard() {
   const leaderboard = makeLeaderboard(PLACES);
   leaderboard.sort((a: Player, b: Player) => b.pins - a.pins);
@@ -81,7 +121,14 @@ export default function Leaderboard() {
               {index + 1}
               {getOrdinals(index)}{' '}
             </div>
-            <div className={styles.name}> {player.author} </div>
+            <div className={styles.name}>
+              {' '}
+              {player.author}{' '}
+              <div className={styles.username}>
+                {' '}
+                {getUsername(index, leaderboard)}{' '}
+              </div>
+            </div>
             <div className={styles.pins}>
               {' '}
               {player.pins} <i className="bi bi-pin-fill"></i>
