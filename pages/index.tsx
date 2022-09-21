b685fb808d6ef3d2e33af6ccb60b63f0e619e896
Diogo Matos <diogomatosemail@gmail.com>
2022-09-21 11:16:55 +0100

    Add title and metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,24 @@
 import dynamic from 'next/dynamic';
 import { ThemeProvider } from '~/hooks/useTheme';
-
+import Head from 'next/head';
 import { PLACES } from '~/data/places';
 
 const Map = dynamic(() => import('~/components/Map'), { ssr: false });
 
 export default function Home() {
   return (
-    <ThemeProvider>
-      <Map pins={PLACES} />
-    </ThemeProvider>
+    <div>
+      <Head>
+        <title>CeSIUM Pelo Mundo</title>
+        <meta
+          name="description"
+          content="A world map with the places we've been"
+          key="description"
+        />
+      </Head>
+      <ThemeProvider>
+        <Map pins={PLACES} />
+      </ThemeProvider>
+    </div>
   );
 }
