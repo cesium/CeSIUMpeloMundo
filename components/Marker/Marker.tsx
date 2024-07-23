7659e816ac59e314847fd748fc5e481bc381de0b
AfonsoMartins26 <afonsopm2005@gmail.com>
2024-07-23 17:49:22 +0100

    Allow to upload larger images without cropping any part of the image

--- components/Marker/Marker.tsx
+++ components/Marker/Marker.tsx
@@ -1,11 +1,10 @@
-import { useMemo } from 'react';
+import { useState, useMemo } from 'react';
 import Image from 'next/image';
-import { Marker as MarkerContainer, Popup } from 'react-leaflet';
+import { Marker as MarkerContainer } from 'react-leaflet';
 import { IPin } from '~/lib/types';
 import { getRelativeTimeString, getNameString, getDistance } from '~/lib/utils';
 import { getIcon, getFullDateString } from './utils';
 import AuthorIcon from '../AuthorIcon';
-
 import styles from './style.module.css';
 
 const Marker = ({
@@ -17,48 +16,57 @@ const Marker = ({
   photo,
   date
 }: IPin) => {
+  const [showModal, setShowModal] = useState(false);
   const icon = getIcon(type);
 
   const name = useMemo(() => getNameString(author), [author]);
 
+  const handleMarkerClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <MarkerContainer
-      icon={icon}
-      position={coordinates}
-      title={`${name} at ${city}`}
-    >
-      <Popup>
-        <div>
-          <div className={styles.background}>
+    <>
+      <MarkerContainer
+        icon={icon}
+        position={coordinates}
+        title={`${name} at ${city}`}
+        eventHandlers={{ click: handleMarkerClick }}
+      />
+
+      {showModal && (
+        <div className={styles.modalOverlay} onClick={handleCloseModal}>
+          <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+            <div className={styles.imageContainer}>
+              <Image
+                alt={`${name} at ${city}`}
+                src={photo}
+                layout="fill"
+                className={styles.image}
+              />
+            </div>
             <div className={styles.text}>
-              <h1 className={styles.title}>
-                {city}, {country}
-              </h1>
-              <span className={styles.light}>
+              <h2 className={styles.title}>{`${city}, ${country}`}</h2>
+              <span className={styles.detail}>
                 <i className="bi bi-calendar"></i> {getFullDateString(date)} (
                 {getRelativeTimeString(date)})
               </span>
-              <br />
-              <span className={styles.light}>
+              <span className={styles.detail}>
                 <i className="bi bi-signpost-fill"></i>{' '}
                 {Math.round(getDistance(coordinates))} km away
               </span>
-              <br />
-              <span>
+              <span className={styles.detail}>
                 <AuthorIcon author={author} /> {name}
               </span>
             </div>
           </div>
-          <Image
-            alt={`${name} at ${city}`}
-            src={photo}
-            layout="fill"
-            objectFit="cover"
-            className={styles.image}
-          />
         </div>
-      </Popup>
-    </MarkerContainer>
+      )}
+    </>
   );
 };
 
